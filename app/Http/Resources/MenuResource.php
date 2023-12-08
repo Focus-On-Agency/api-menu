0069784740104552c4bc0f6f74e704cf4835bba7
pcatapan <pacho30000@gmail.com>
2023-12-08 17:25:06 +0100

    fix menu

--- app/Http/Resources/MenuResource.php
+++ app/Http/Resources/MenuResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Menu;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,12 +15,18 @@ class MenuResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $menu = Menu::find($this->id);
+
         return [
             'id' => $this->id,
             'name' => $this->name,
             'icon' => $this->icon_name,
             'categories' => CategoryResource::collection($this->whenLoaded('categories')),
-            'dishes' => DishResource::collection($this->whenLoaded('dishes')),
+            'dishes' => $this->whenLoaded('dishes', function () use ($menu) {
+                return $this->dishes->map(function ($dish) use ($menu) {
+                    return new DishResource($dish, $menu);
+                });
+            }),
         ];
     }
 }
