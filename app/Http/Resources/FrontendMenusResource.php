54c35a2c9eafc1b50ae541d4c8f5c6eafb07fc1c
pcatapan <pacho30000@gmail.com>
2023-12-11 00:37:51 +0100

    fix frontend menu

--- app/Http/Resources/FrontendMenusResource.php
+++ app/Http/Resources/FrontendMenusResource.php
@@ -15,6 +15,7 @@ class FrontendMenusResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'icon' => $this->icon_name,
         ];
